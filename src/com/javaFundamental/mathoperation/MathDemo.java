47f576f0ecd317329f597a28ba24aa2449cc0e9d
Divyanshu kumar <Divyanshuk9060@gmail.com>
2023-09-15 23:20:44 +0530

    "Math.Demo.java: all methods"

--- src/com/javaFundamental/mathoperation/MathDemo.java
+++ src/com/javaFundamental/mathoperation/MathDemo.java
@@ -4,5 +4,6 @@ public class MathDemo {
     public static void main(String[] args) {
         System.out.println(Math.max(5, 10));
         System.out.println(Math.min(5, 10));
+        System.out.println(Math.ceil(6.44));
     }
 }
