222f3622687ca467e4fbb3103059637bc6b377da
Divyanshu kumar <Divyanshuk9060@gmail.com>
2023-09-15 22:50:24 +0530

    "VariableDemo.java commit"

---
+++ src/com/javaFundamental/variables/VariablesDemo.java
@@ -0,0 +1,21 @@
+package com.javaFundamental.variables;
+
+public class VariablesDemo {
+    int age=10;
+    float salary=10000.54f;
+    double highSalary=544434.09890;
+    char alphabets='A';
+    String name="Divyanshu";
+    short digit=5;
+
+    public static void main(String[] args) {
+        VariablesDemo variablesDemo= new VariablesDemo();
+        System.out.println(variablesDemo.age);
+        System.out.println(variablesDemo.salary);
+        System.out.println(variablesDemo.highSalary);
+        System.out.println(variablesDemo.alphabets);
+        System.out.println(variablesDemo.name);
+        System.out.println(variablesDemo.digit);
+
+    }
+}
