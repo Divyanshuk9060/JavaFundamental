00d739d16377c4342cd0076589ec5c06dab08ca8
Divyanshu kumar <Divyanshuk9060@gmail.com>
2023-09-15 22:35:15 +0530

    Commit for Comment: JavaCommentDemo.java

---
+++ src/com/javaFundamental/comments/JavaCommentDemo.java
@@ -0,0 +1,16 @@
+package com.javaFundamental.comments;
+
+public class JavaCommentDemo {
+    public static void main(String[] args) {
+        //Single line comment
+        /*multiline comments
+        *
+        *
+        * */
+        System.out.println("// .....// is used for single line comments");
+        System.out.println("/*multiline comments\n" +
+                "        * \n" +
+                "        * \n" +
+                "        * */ multiline comments");
+    }
+}
