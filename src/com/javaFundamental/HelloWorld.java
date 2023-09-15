9abe5765437e724961fe99d591803a6b0994eb2d
Divyanshu kumar <Divyanshuk9060@gmail.com>
2023-09-15 22:31:27 +0530

    "Comments is java: JavaCommentsDemo.java "

--- src/com/javaFundamental/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package com.javaFundamental;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("HelloWorld");
-        System.out.println("Hello");
-    }
-}
--- src/com/javaFundamental/lteral/LiteralTest.java
+++ src/com/javaFundamental/lteral/LiteralTest.java
@@ -9,3 +9,4 @@ public class LiteralTest {
         System.out.println(myNumber);
     }
 }
+
