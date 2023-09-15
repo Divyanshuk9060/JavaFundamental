48842522cb6cc34f09d9c9f8b5facc5ce61761b5
Divyanshu kumar <Divyanshuk9060@gmail.com>
2023-09-15 13:22:00 +0530

    commit

---
+++ src/com/javaFundamental/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.javaFundamental;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+        System.out.println("Hello");
+    }
+}
