107759713363910e760d8a10335d3c5265619bd7
Divyanshu kumar <Divyanshuk9060@gmail.com>
2023-09-15 23:16:35 +0530

    "Math.Demo.java: all methods"

--- src/com/javaFundamental/stringopr/StringDemo.java
+++ src/com/javaFundamental/stringopr/StringDemo.java
@@ -4,7 +4,7 @@ public class StringDemo {
     public static void main(String[] args) {
         String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         //txt.length() method is used for checking the length of a string.
-        System.out.println("The length of the txt string is: " + txt.length());
+        System.out.println("MThe length of the txt string is: " + txt.length());
         System.out.println(txt.toLowerCase());   // Outputs "hello world"
         System.out.println(txt.toUpperCase());   // Outputs "HELLO WORLD"
         String txts = "Please locate where 'locate' occurs!";
