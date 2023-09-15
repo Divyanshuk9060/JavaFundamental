fbe219cc9668bd4f477ab2b800ff6b91b335b0d5
Divyanshu kumar <Divyanshuk9060@gmail.com>
2023-09-15 16:56:25 +0530

    "Commit to Add Literal.java"

---
+++ src/com/javaFundamental/lteral/LiteralTest.java
@@ -0,0 +1,11 @@
+package com.javaFundamental.lteral;
+
+public class LiteralTest {
+    public static void main(String[] args) {
+        //How to create a valid literal in java
+        String  name="Divyanshu";
+        int myNumber=30;
+        System.out.println(name);
+        System.out.println(myNumber);
+    }
+}
