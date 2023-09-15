a91ecdabbf50869f9351fc42fd9592b93d3fbaad
Divyanshu kumar <Divyanshuk9060@gmail.com>
2023-09-15 22:59:35 +0530

    "StringDemo.java:String length() commit"

---
+++ src/com/javaFundamental/StringDemo.java
@@ -0,0 +1,9 @@
+package com.javaFundamental;
+
+public class StringDemo {
+    public static void main(String[] args) {
+        String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        //txt.length() method is used for checking the length of a string.
+        System.out.println("The length of the txt string is: " + txt.length());
+    }
+}
